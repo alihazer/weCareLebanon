453a7735abfe9951bce2e1cdfe599d4141061aa6
Hussein Nahal <nahalhusssein1000@gmail.com>
2025-01-11 13:36:14 +0200

    add product

--- public/js/product.js
+++ public/js/product.js
@@ -47,9 +47,88 @@ async function getsupplier() {
         alert('An error occurred while loading categories.');
     }
 }
+
+function handleok() {
+    document.getElementById('messageproduct').style.display = 'none';
+}
+
 if (window.location.pathname=="/products/add") {
      getCategories();    
      getsupplier();
+
+
+       document.getElementById('addProduct').addEventListener('submit', async function (e) {
+            e.preventDefault();
+    
+            const name = document.getElementById('name').value.trim();
+            const details = document.getElementById('details').value.trim();
+            const code = document.getElementById('code').value.trim();
+            const quantity = parseInt(document.getElementById('quantity').value, 10);
+            const purchasePrice = parseFloat(document.getElementById('purchasePrice').value);
+            const wholeSalePrice = parseFloat(document.getElementById('wholeSalePrice').value);
+            const singlePrice = parseFloat(document.getElementById('singlePrice').value);
+            const category_id = document.getElementById('category_id').value.trim();
+            const supplierId = document.getElementById('supplierId').value.trim();
+            const image = document.getElementById('image').files[0];
+            
+            if (!name || !code ||isNaN(quantity) || isNaN(purchasePrice) || isNaN(wholeSalePrice) || isNaN(singlePrice) || !category_id || !supplierId || !image) {
+                document.getElementById('messageproduct').style.display = 'flex';
+                const warningText = document.getElementById('message');
+                warningText.textContent = 'All fields are required';
+                return;
+            }
+            
+            else if (name.length < 3) {
+                document.getElementById('messageproduct').style.display = 'flex';
+                const warningText = document.getElementById('message');
+                warningText.textContent = 'name must be at least 3 characters';
+                return;
+            }
+            else if (code.length < 3) {
+                document.getElementById('messageproduct').style.display = 'flex';
+                const warningText = document.getElementById('message');
+                warningText.textContent = 'code must be at least 3 characters';
+                return;
+            }
+            else if (purchasePrice >=singlePrice || purchasePrice >=wholeSalePrice) {
+                document.getElementById('messageproduct').style.display = 'flex';
+                const warningText = document.getElementById('message');
+                warningText.textContent = 'Purchase price must be less than both single price and wholesale price';
+                return;
+            }
+
+
+
+            try {
+                const response = await fetch('/api/products/add', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ name, category_id, code, details, purchasePrice, wholeSalePrice, singlePrice, supplierId, quantity, image  }),
+                });
+                
+                const result = await response.json();
+    
+                if (response.ok) {
+                    document.getElementById('messageproduct').style.display = 'flex';
+                    const succ = document.getElementById('message');
+                    succ.textContent = result.message;
+                    document.getElementById('addProduct').reset();
+                    console.log(result);
+                    
+                } else {
+                    document.getElementById('messageproduct').style.display = 'flex';
+                    const warningText = document.getElementById('message');
+                    warningText.textContent = result.errors;
+                    console.log(result.errors);
+
+                }
+            } catch (error) {
+                alert('An error occurred while adding the supplier.');
+            }
+        });
+    
 }
 
 
