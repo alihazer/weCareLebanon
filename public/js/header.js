0ce635da9498fca4b29e86cddff9c7ed23e9daf2
Hussein Nahal <nahalhusssein1000@gmail.com>
2025-01-16 23:26:03 +0200

    add invoice conttainer

--- public/js/header.js
+++ public/js/header.js
@@ -3,10 +3,10 @@ document.querySelector('.menuIcon').addEventListener('click', () => {
     document.querySelector('.menuContainer').style.left='0';
 });
 document.querySelector('.close').addEventListener('click', () => {
-    document.querySelector('.menuContainer').style.left = '-100%';
+    document.querySelector('.menuContainer').style.left = '-170%';
 });
 document.querySelector('.closeContainer').addEventListener('click', () => {
-    document.querySelector('.menuContainer').style.left = '-100%';
+    document.querySelector('.menuContainer').style.left = '-170%';
 });
 
 
